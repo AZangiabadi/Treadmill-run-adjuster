9e93e59e85e1c9f66dc377cc6d87dc76b3f7a104
AZangiabadi <79270542+AZangiabadi@users.noreply.github.com>
2022-12-06 17:20:38 -0500

    Update pace-adjust.py

--- pace-adjust.py
+++ pace-adjust.py
@@ -1,16 +1,30 @@
 import re
-file = open(os.path.expanduser("~/Downloads/Evening_Run1.tcx"), 'r')
-for line in file:
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
+temp_file_path = file_path.replace(".tcx", "_corrected.tcx")
+
+correct_distance = float(input('What is the shown distance on your treadmill (the correct one)?\n'))
+wrong_distance = float(input('What is the shown distance on your smartwatch (the wrong one)?\n'))
+
+correction_ratio = correct_distance / wrong_distance
+
+file1 = open(file_path, 'r')
+file2 = open(temp_file_path, 'w')
+
+for line in file1:
     if re.match("            <DistanceMeters>", line):
-        aa=re.findall(r'\d+\.\d+', line)
-        bb=float(aa[0])*1.2
-        print(bb)
-        file.write(line = "            <DistanceMeters>"+str(bb)+"</DistanceMeters>")
-    file.write(line)
-#contents = file.read()
+        distance=re.findall(r'\d+\.\d+', line)
+        distance1=str(float(distance[0]))
+        distance_corrected=float(distance[0])*correction_ratio
+        distance2=str(distance_corrected)
+        file2.write(line.replace(distance1, distance2))
+    else:
+        file2.write(line)
 
-file.close()
-
-with open(os.path.expanduser("~/Downloads/Evening_Run1.tcx"), 'r') as file :
-  filedata = file.read()
-print(filedata)
+file1.close()
+file2.close()
